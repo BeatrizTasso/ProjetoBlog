e33b6f0ea9d14d7a93b7d1b0e5bc0cc57baede9b
BeatrizTasso <beatriztasso1234@gmail.com>
2023-08-15 11:48:53 -0300

    15/08

--- banco_blog.sql
+++ banco_blog.sql
@@ -1,22 +1,34 @@
+drop database if exists blog;
+create database blog;
 use blog;
 
-create table usuario(
-id int not null auto_increment, 
-nome varchar(50) not null,
-email varchar(255) not null,
-senha varchar(60) not null,
-data_criacao datetime not null default current_timestamp,
-ativo tinyint not null default'0',
-adm tinyint not null default '0',
-primary key (id));
+CREATE TABLE usuario (
+id int NOT NULL AUTO_INCREMENT,
+email varchar(255) NOT NULL,
+senha varchar(60) NOT NULL,
+nome varchar(50) NOT NULL, data_criacao datetime NOT NULL DEFAULT CURRENT_TIMESTAMP, ativo tinyint NOT NULL DEFAULT '0', adm tinyint NOT NULL DEFAULT '0',
+PRIMARY KEY (id)
+);
 
-create table post(
-id int not null auto_increment, 
-titulo varchar(255) not null,
-texto text not null, 
-usuario_id int not null, 
-data_criacao datetime not null default current_timestamp,
-data_postagem datetime not null, 
-primary key (id), 
-key fk_post_usuario_idx (usuario_id),
-constraint fk_post_usuario foreign key (usuario_id) references usuario (id));
+CREATE TABLE post (
+id int NOT NULL AUTO_INCREMENT,
+titulo varchar(255) NOT NULL,
+texto text NOT NULL,
+usuario_id int NOT NULL,
+data_criacao datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+data_postagem datetime NOT NULL,
+PRIMARY KEY (id),
+KEY fk_post_usuario_idx (usuario_id),
+CONSTRAINT Fk_post_usuario FOREIGN KEY (usuario_id) REFERENCES usuario (id)
+);
+CREATE TABLE avaliacao (
+id int NOT NULL AUTO_INCREMENT,
+nota int NOT NULL,
+comentario varchar(255) NOT NULL,
+usuario_id int NOT NULL,
+post_id int NOT NULL,
+data_criacao datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+PRIMARY KEY (id),
+CONSTRAINT fk_avaliacao_usuario FOREIGN KEY (usuario_id) REFERENCES usuario (id),
+CONSTRAINT fk_avaliacao_post FOREIGN KEY (post_id) REFERENCES post (id)
+);
